376e4ac6dd9f66421e2989e95a3b1ebcfbc1338a
d-pamneja <dpamneja@gmail.com>
2024-08-09 15:47:40 +0530

    Simple Shapes Research Model

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hyphen_e_dot = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+    return requirements
+
+setup(
+    name='Curvetopia_DA',
+    version="0.0.1",
+    author="Dhruv Pamneja & Abhay Chaudhary",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
---
+++ src/generate_dataset.py
@@ -0,0 +1,74 @@
+# Importing the dependencies
+import numpy as np
+import cv2
+import os
+
+# Defining the function to generate the dataset
+def generate_custom_shapes_dataset(num_samples_per_class=1000, output_directory='./dataset'):
+    """
+    Generate a dataset of custom shapes.
+    
+    Parameters:
+    - num_samples_per_class: Number of samples to generate per class.
+    - output_dir: Directory to save the generated dataset.
+    
+    Returns:
+    - None
+    """
+    
+    shapes = ['line', 'circle', 'ellipse', 'rectangle', 'rounded_rectangle', 'regular_polygon', 'star']
+    if(not os.path.exists(output_directory)):
+        os.makedirs(output_directory)
+    
+    for shape in shapes:
+        shape_directory = os.path.join(output_directory, shape)
+        if(not os.path.exists(shape_directory)):
+            os.makedirs(shape_directory)
+        
+        for i in range(num_samples_per_class):
+            img = np.zeros((128, 128), dtype=np.uint8)
+            
+            if(shape == 'line'):
+                x1, y1 = np.random.randint(0, 128, size=2)
+                x2, y2 = np.random.randint(0, 128, size=2)
+                cv2.line(img, (x1, y1), (x2, y2), 255, 2)
+            
+            elif(shape == 'circle'):
+                radius = np.random.randint(10, 40)
+                x, y = np.random.randint(radius, 128-radius, size=2)
+                cv2.circle(img, (x, y), radius, 255, 2)
+            
+            elif(shape == 'ellipse'):
+                x, y = np.random.randint(20, 108, size=2)
+                axes = np.random.randint(10, 50, size=2)
+                angle = np.random.randint(0, 180)
+                cv2.ellipse(img, (x, y), tuple(axes), angle, 0, 360, 255, 2)
+            
+            elif(shape == 'rectangle'):
+                x1, y1 = np.random.randint(0, 108, size=2)
+                x2, y2 = x1 + np.random.randint(20, 40), y1 + np.random.randint(20, 40)
+                cv2.rectangle(img, (x1, y1), (x2, y2), 255, 2)
+            
+            elif(shape == 'rounded_rectangle'):
+                x1, y1 = np.random.randint(0, 108, size=2)
+                x2, y2 = x1 + np.random.randint(20, 40), y1 + np.random.randint(20, 40)
+                radius = np.random.randint(5, 15)
+                cv2.rectangle(img, (x1, y1), (x2, y2), 255, 2)
+                cv2.circle(img, (x1, y1), radius, 255, 2)
+                cv2.circle(img, (x2, y2), radius, 255, 2)
+                cv2.circle(img, (x1, y2), radius, 255, 2)
+                cv2.circle(img, (x2, y1), radius, 255, 2)
+            
+            elif(shape == 'regular_polygon'):
+                points = np.random.randint(0, 128, size=(5, 2))
+                cv2.polylines(img, [points], isClosed=True, color=255, thickness=2)
+            
+            elif(shape == 'star'):
+                points = np.random.randint(0, 128, size=(5, 2))
+                cv2.polylines(img, [points], isClosed=True, color=255, thickness=2)
+            
+            img_path = os.path.join(shape_directory, f'{i}.png')
+            cv2.imwrite(img_path, img)
+
+
+generate_custom_shapes_dataset()
